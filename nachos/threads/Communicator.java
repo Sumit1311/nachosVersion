91e0bef8fd4e52abfddbb66b1c14ab8fb37bc10d
Sumit Toshniwal <sumittoshniwal92@gmail.com>
2014-12-21 08:40:41 -0800

    Removed proj4

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -9,11 +9,19 @@ import nachos.machine.*;
  * be a time when both a speaker and a listener are waiting, because the two
  * threads can be paired off at this point.
  */
-public class Communicator {
+public class Communicator
+{
     /**
      * Allocate a new communicator.
      */
-    public Communicator() {
+    public Communicator()
+    {
+        lock = new Lock();
+        condition = new Condition2(lock);
+        message = 0;
+        flag = false;
+        listenerComplete = false;
+        speakerComplete = false;
     }
 
     /**
@@ -24,18 +32,219 @@ public class Communicator {
      * Does not return until this thread is paired up with a listening thread.
      * Exactly one listener should receive <i>word</i>.
      *
-     * @param	word	the integer to transfer.
+     * @param   word    the integer to transfer.
      */
-    public void speak(int word) {
+    public void speak(int word)
+    {
+                Lib.debug(debug, "Beginning of speak method");
+
+                if (!flag)
+                {
+                        if (!lock.isHeldByCurrentThread())
+                        {
+                                lock.acquire();
+                        }
+                        flag = true;
+                        Lib.debug(debug, "Speak ready, waiting for listener, going to sleep");
+                        condition.sleep();
+
+                        Lib.debug(debug, "Listener ready, speaker is continuing and going to send the word");
+                        message = word;
+                }
+                else
+                {
+                        Lib.debug(debug, "Listener ready, speaker is sending word");
+                        message = word;
+                }
+
+                Lib.debug(debug, "Word sent, speaker attempting to notify the listener");
+               
+                if (!lock.isHeldByCurrentThread())
+                {
+                        lock.acquire();
+                }
+               
+                condition.wake();
+               
+                //if the listener is not complete, wait to exit until listener has received
+                //the word the speaker sent
+                if (!listenerComplete)
+                {
+                        if (!lock.isHeldByCurrentThread())
+                        {
+                                lock.acquire();
+                        }
+                       
+                        condition.sleep();
+                }
+               
+                //Speaker will now finish and wake the listener so he also can finish
+                if (!lock.isHeldByCurrentThread())
+                {
+                        lock.acquire();
+                }
+                speakerComplete=true;
+               
+                condition.wake();
+               
+                if (lock.isHeldByCurrentThread())
+                {
+                        lock.release();
+                }      
+
+                Lib.debug(debug, "Speaker FINISHED");
     }
 
     /**
      * Wait for a thread to speak through this communicator, and then return
      * the <i>word</i> that thread passed to <tt>speak()</tt>.
      *
-     * @return	the integer transferred.
-     */    
-    public int listen() {
-	return 0;
+     * @return  the integer transferred.
+     */
+    public int listen()
+    {
+                Lib.debug(debug, "Beginning of listen method");
+
+                if (flag)
+                {
+                        Lib.debug(debug, "Listener ready, proceeding to try to listen to message from speak");
+                       
+                        if (!lock.isHeldByCurrentThread())
+                        {
+                                lock.acquire();
+                        }
+
+                        condition.wake();
+
+                        Lib.debug(debug, "listener has called to wake speaker, now going to sleep");
+                        if (!lock.isHeldByCurrentThread())
+                        {
+                                lock.acquire();
+                        }
+
+                        condition.sleep();
+
+                        Lib.debug(debug, "listener has waken up after speaker finished");
+                }
+                else
+                {
+                        flag = true;
+                        Lib.debug(debug, "Listener ready, waiting for speaker");
+                        if (!lock.isHeldByCurrentThread())
+                        {
+                                lock.acquire();
+                        }
+
+                        condition.sleep();
+                }
+
+                flag = false;
+                Lib.debug(debug, "Listener received word, waking speaker so he can exit");
+                if (!lock.isHeldByCurrentThread())
+                {
+                        lock.acquire();
+                }
+
+                condition.wake();
+               
+                //sleep till speaker has exited
+                if (!lock.isHeldByCurrentThread())
+                {
+                        lock.acquire();
+                }
+
+                condition.sleep();
+               
+                if (lock.isHeldByCurrentThread())
+                {
+                        lock.release();
+                }
+               
+                if (speakerComplete)
+                {
+                        //reset variables and exit
+                    flag = false;
+                    listenerComplete = false;
+                    speakerComplete = false;    
+                }
+               
+                Lib.debug(debug, "Listener FINISHED");
+                return message;
+        }
+
+
+    private static class CommunicatorTest implements Runnable {
+        CommunicatorTest(Communicator communicator, int type) {
+            this.communicator = communicator;
+            this.type = type;
+        }
+
+        public void run()
+        {
+                int receivedNumber = 0;
+
+                if (1 == type) //speaker
+                {              
+                        communicator.speak(1);
+                        Lib.debug(debug, "1st speak call sent " + 1);
+                       
+                        communicator.speak(2);
+                        Lib.debug(debug, "2nd speak call sent " + 2);
+                       
+                        communicator.speak(3);
+                        Lib.debug(debug, "3rd speak call sent " + 3);
+                       
+                        communicator.speak(4);
+                        Lib.debug(debug, "4th speak call sent " + 4);
+                       
+                        communicator.speak(5);
+                        Lib.debug(debug, "5th speak call sent " + 5);
+                }
+                else //listener
+                {
+                        receivedNumber = communicator.listen();
+                        Lib.debug(debug, "1st listen call returned " + receivedNumber);
+
+                        receivedNumber = communicator.listen();
+                        Lib.debug(debug, "2nd listen call returned " + receivedNumber);
+
+                        receivedNumber = communicator.listen();
+                        Lib.debug(debug, "3rd listen call returned " + receivedNumber);
+
+                        receivedNumber = communicator.listen();
+                        Lib.debug(debug, "4th listen call returned " + receivedNumber);
+
+                        receivedNumber = communicator.listen();
+                        Lib.debug(debug, "5th listen call returned " + receivedNumber);
+                }
+        }
+
+        private Communicator communicator;
+        private int type;
     }
+
+    /**
+     * Test if this module is working.
+     */
+    public static void selfTest() {
+        Communicator commObject = new Communicator();
+        Communicator commObject2 = new Communicator();
+
+        new KThread(new CommunicatorTest(commObject, 1)).setName("speaker").fork();
+        new KThread(new CommunicatorTest(commObject, 2)).setName("listener").fork();
+       
+        new KThread(new CommunicatorTest(commObject2, 2)).setName("listener").fork();          
+        new KThread(new CommunicatorTest(commObject2, 1)).setName("speaker").fork();
+       
+    }
+
+    private Lock lock;
+    private static final char debug = 't';
+    int message;
+    Condition2 condition;
+    boolean flag;
+    boolean listenerComplete;
+    boolean speakerComplete;
+
 }
+
